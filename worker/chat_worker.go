7989d78d27ae898408b7c56b86667471d2a4ca9c
mr-shitij <21.shitijagrawal@gmail.com>
2024-07-12 11:59:16 +0530

    bug fix

--- worker/chat_worker.go
+++ worker/chat_worker.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/redis/go-redis/v9"
+	"strconv"
 	"sync-worker/database/services"
 	"sync-worker/helpers"
 	"time"
@@ -16,7 +17,12 @@ func SyncChatDatabase(ctx context.Context, database *services.Database, val *red
 	chats := val.Values["chats"].(string)
 	chatsSummary := val.Values["chatsSummary"].(string)
 	isNew := val.Values["isNew"].(string)
-	balance := val.Values["balance"].(float64)
+	balanceStr := val.Values["balance"].(string)
+
+	balance, err := strconv.ParseFloat(balanceStr, 64)
+	if err != nil {
+		return err
+	}
 
 	dataContext, cancel := context.WithDeadline(ctx, time.Now().Add(time.Minute))
 	errData := database.AddChat(dataContext, sessionId, sessionPrompt, chats, chatsSummary, isNew)
